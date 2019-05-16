cb5acca3fc030a318227b43d1b76ef59e22558c7
Jason Li <38285672+WardBot@users.noreply.github.com>
2019-05-16 01:00:26 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -4,8 +4,9 @@ function myFunction() {
   var third =document.getElementById("grades").elements["Three"].value;
   var fourth =document.getElementById("grades").elements["Four"].value;
   var sum = parseInt(first) + parseInt(second) + parseInt(third) + parseInt(fourth);
+  sum-= Math.min(parseInt(first), parseInt(second), parseInt(third), parseInt(fourth))
   var avg = sum/3;
-  document.getElementById("GetAverage").innerHTML = avg;
+  document.getElementById("getAverage").innerHTML = avg;
   if(avg>=90) document.getElementById("LetterGrade").innerHTML = "A";
   else if (avg>=80 & avg<90) document.getElementById("LetterGrade").innerHTML = "B";
   else if (avg>=70 & avg<80) document.getElementById("LetterGrade").innerHTML = "C";
